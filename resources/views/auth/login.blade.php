4c48d6f6157c76bbdfb396752e7a7401d90dca44
Evandro <professorevandroaurelio@gmail.com>
2026-05-28 15:41:25 -0300

    corrigindo o icone da aba do navegador

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Login | Administrar</title>
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg">
   <!-- Bootstrap 5 CDN -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
